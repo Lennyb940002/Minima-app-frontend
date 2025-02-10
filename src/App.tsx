5977d7f6bed0caeacc7cc4dd637baae8673ec363
Lennyb940002 <146711861+Lennyb940002@users.noreply.github.com>
2025-02-10 15:34:03 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -128,3 +128,14 @@ function App() {
 }
 
 export default App;
+
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
